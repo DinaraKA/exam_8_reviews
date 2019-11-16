dfa35fd158506373b2b3c9def483fc0cd1c4240d
DinaraKA <d.karybekova@gmail.com>
2019-11-16 12:36:53 +0600

    Added model product. Installed Pillow, widget_tweaks.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description')
+    list_filter = ('category',)
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('other', 'Other'),
+    ('book', 'Book'),
+    ('hotel', 'Hotel'),
+    ('car', 'Car')
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Product')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Category')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    image = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Image')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
