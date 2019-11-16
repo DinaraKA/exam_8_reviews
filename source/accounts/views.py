be25ae7ff4f0492f7e36f04d189ff4ad283a3ca0
DinaraKA <d.karybekova@gmail.com>
2019-11-16 16:02:42 +0600

    Added user registration. Added login requirements in settings.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from accounts.forms import UserCreationForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email']
+                )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', context={'form': form})
+
+
+
