d750c2b8d14054c02b867e09e620103e9c9eef53
DinaraKA <d.karybekova@gmail.com>
2019-11-16 18:20:43 +0600

    Corrected related name

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -53,12 +53,12 @@ class UserInfoChangeForm(forms.ModelForm):
 
     def get_initial_for_field(self, field, field_name):
         if field_name in self.Meta.profile_fields:
-            return getattr(self.instance.profile, field_name)
+            return getattr(self.instance.user_profile, field_name)
         return super().get_initial_for_field(field, field_name)
 
     def save(self, commit=True):
         user = super().save(commit=commit)
-        user.profile = self.save_profile(commit)
+        user.user_profile = self.save_profile(commit)
         return user
 
     def save_profile(self, commit=True):
@@ -68,7 +68,6 @@ class UserInfoChangeForm(forms.ModelForm):
         if commit:
             profile.save()
         return profile
-
 
 
     class Meta:
