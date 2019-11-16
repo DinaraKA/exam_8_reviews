bb7cb25764144acec4d54be432ca3878d21735b1
DinaraKA <d.karybekova@gmail.com>
2019-11-16 14:52:10 +0600

    Added product create.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from webapp.views import IndexView, ProductView
+from webapp.views import IndexView, ProductView, ProductCreateView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('task/<int:pk>/', ProductView.as_view(), name='product_detail'),
+    path('task/add/', ProductCreateView.as_view(), name='product_add'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
 from webapp.models import Product
 
@@ -8,7 +10,17 @@ class IndexView(ListView):
     context_object_name = 'products'
     template_name = 'index.html'
 
+
 class ProductView(DetailView):
     model = Product
     template_name = 'product/detail.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    template_name = 'product/product_create.html'
+    fields = ['image', 'name', 'category', 'description']
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
