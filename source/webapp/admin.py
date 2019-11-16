2c271014998a67fe6e9c10f348f0de9132a29ffb
DinaraKA <d.karybekova@gmail.com>
2019-11-16 13:00:43 +0600

    Added model Review.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from webapp.models import Product
+from webapp.models import Product, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'category', 'description')
     list_filter = ('category',)
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'product', 'text', 'rating')
+    list_filter = ('author','product', 'rating')
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -22,3 +23,28 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+
+
+RATING_CHOICES = (
+    (5, 'excellent'),
+    (4, 'good'),
+    (3, 'so-so'),
+    (2, 'bad'),
+    (1, 'awful')
+)
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, related_name='user_review', on_delete=models.PROTECT, verbose_name='Author')
+    product = models.ForeignKey(Product, related_name='product_review', on_delete=models.PROTECT,
+                                    verbose_name='Product')
+    text = models.TextField(max_length=1000, verbose_name='Text')
+    rating = models.IntegerField(choices=RATING_CHOICES, verbose_name='Rating')
+
+
+    def __str__(self):
+        return str(self.author.username)
+
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
