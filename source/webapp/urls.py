dd40da5a5c588ad0232df762dfaf285ad791c0a1
DinaraKA <d.karybekova@gmail.com>
2019-11-16 14:26:52 +0600

    Added detail view of product.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from webapp.views import IndexView
+from webapp.views import IndexView, ProductView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('task/<int:pk>/', ProductView.as_view(), name='product_detail'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 from webapp.models import Product
 
 
@@ -6,3 +7,8 @@ class IndexView(ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'index.html'
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/detail.html'
+    context_object_name = 'product'
